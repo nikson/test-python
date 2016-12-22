9ba27f409b6771b014eb79611bcf86df4e402b5a
nikson <nikson.sust@gmail.com>
2016-12-23 00:51:27 +0100

    Find N missing value in a list

---
+++ find_n_missing_value.py
@@ -0,0 +1,14 @@
+
+# a = [ 1, 2, 3, 5, 6, 7, 8, 10]
+#    find_N_missing_number(a, 10)
+def find_N_missing_number(data, total):    
+    if isinstance(data, list):
+        x = [0] * total
+        for d in data: x[d-1] = 1        
+        for (index, val) in enumerate(x, start=1):            
+            if val == 0: print(index)
+            #print(index, val)
+
+if __name__ == "__main__":
+    a = [ 1, 2, 3, 5, 6, 7, 8, 10]
+    find_N_missing_number(a, 10)
